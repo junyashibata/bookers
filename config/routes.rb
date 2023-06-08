47510e26bb046221649240d60bc1369d3be179d5
JunyaShibata <135484706+junyashibata@users.noreply.github.com>
2023-06-08 11:37:10 +0000

    make base 3-kadai

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'lists/new'
+  get 'lists/show'
+  get 'lists/edit'
+  get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
